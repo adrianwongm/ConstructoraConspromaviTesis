d7a42636782180e33a5c258b3c7dc8ef1d997893
adrianowm <Adrianowm@hotmail.com>
2017-03-22 23:52:26 -0500

    Inicio de solucion

---
+++ HotelBooking.Desktop/App_Code/SearchUsuario.cs
@@ -0,0 +1,47 @@
+﻿using HotelBooking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de SearchUsuario
+/// </summary>
+public class SearchUsuario
+{
+    public SearchUsuario()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+
+    public SearchUsuario(string page, HotelBookingDataContext context)
+    {
+        Page = page;
+        DataContext = context;
+        
+    }
+
+    public string Page { get; set; }
+
+    public HotelBookingDataContext DataContext { get; set; }
+
+    public Usuarios verficaUsuario(string pEmail, string pPassword)
+    {
+        Usuarios userCliente = null;
+        try
+        {
+          userCliente =   DataContext.Usuarios.Where(p => p.Usuario == pEmail && p.Password == pPassword).FirstOrDefault();
+            if(userCliente == null)
+            {
+                throw new Exception("No existe el usuario");
+            }
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+        return userCliente;
+    }
+}
---
+++ HotelBooking.Desktop/UserControls/HeaderControl.ascx.cs
@@ -0,0 +1,26 @@
+﻿using HotelBooking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class UserControls_HeaderControl : System.Web.UI.UserControl
+{
+    public SearchUsuario Usuario { get; set; }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnLoginNow_Click(object sender, EventArgs e) {
+        if(Captcha.IsValid)
+            LogonControl.ShowOnPageLoad = false;
+        //Carga de controles
+        Usuario = new SearchUsuario(this.ToString(), new HotelBooking.HotelBookingDataContext());
+        Usuarios user  = Usuario.verficaUsuario(this.txtEmail.Text.Trim(), this.txtPassword.Text.Trim());
+        Session["userCliente"] = user.Usuario;
+
+
+    }
+}
